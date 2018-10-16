692bf709d94c2ad5db0c3eb471a55d888cf13602
Swarnim Das <1405266@kiit.ac.in>
2018-10-16 14:51:16 +0530

    Updated README.md

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 #Server for Onboarding MicroService
 
-running on port 4000
+##Running on port 4000
+
+###Pre-requisites:
+
+> >run the sql script 'create-tables.sql' by typing the following command in mysql shell
+
+> >SOURCE create-tables.sql;
+
+>> run npm install
+
---
+++ create-tables.sql
@@ -0,0 +1,14 @@
+DROP DATABASE `agiler`;
+CREATE SCHEMA `agiler` ;
+CREATE TABLE `agiler`.`user` (
+  `id` VARCHAR(100) NOT NULL,
+  `name` VARCHAR(100) NULL,
+  `email` VARCHAR(100) NULL,
+  `profilePicUrl` VARCHAR(345) NULL,
+  PRIMARY KEY (`id`));
+CREATE TABLE `agiler`.`team` (
+  `teamId` VARCHAR(100) NOT NULL,
+  `teamName` VARCHAR(100) NULL,
+  `memberId` VARCHAR(100) NOT NULL,
+  `projectId` VARCHAR(100) NOT NULL,
+  PRIMARY KEY (`teamId`, `memberId`, `projectId`));
